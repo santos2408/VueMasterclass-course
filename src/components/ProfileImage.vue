432c3fe6a7c1db0539bf614cac4a72c977dc43f8
Roger Santos <roger.santos36@gmail.com>
2023-06-25 01:06:48 -0300

    created profileImage and actionButtob components

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <img
+    :src="url"
+    alt="User profile image"
+    class="h-10 w-10 rounded-3xl object-contain"
+  />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      url: "https://i.pravatar.cc/300",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
 
 import MainNav from "@/components/MainNav.vue";
 
@@ -7,5 +8,26 @@ describe("MainNav", () => {
     render(MainNav);
     const companyName = screen.getByText("Bobo Careers");
     expect(companyName).toBeInTheDocument();
+  });
+
+  describe("when the user logs in", () => {
+    it("displays user profile picture", async () => {
+      render(MainNav);
+
+      let profileImage = screen.queryByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).not.toBeInTheDocument();
+
+      const loginButton = screen.getByRole("button", {
+        name: /Sign In/i,
+      });
+      await userEvent.click(loginButton);
+
+      profileImage = screen.queryByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).toBeInTheDocument();
+    });
   });
 });
