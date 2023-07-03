7e2c80c243f43219980be1ece38a8a0419fe3868
Roger Santos <roger.santos36@gmail.com>
2023-07-03 17:50:21 -0300

    config router

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRoute, createWebHashHistory } from "vue-route";
+
+import HomeView from "@/views/HomeView.vue";
+import JobResultsView from "@/views/JobResultsView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomeView,
+  },
+  {
+    path: "/jobs/results",
+    name: "JobResults",
+    component: JobResultsView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory,
+  routes,
+});
+
+export default router;
---
+++ src/views/JobResultsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Job results page</h1>
+</template>
+
+<script>
+export default {
+  name: "JobResultsView",
+};
+</script>
